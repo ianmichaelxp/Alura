d4b3d7e95af2a95c2455b4f607714eab7cc57dac
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-10-10 16:08:35 -0300

    Aula 08

---
+++ ALURA - JPA2 & HIBERNATE/src/br/alura/com/financas/teste/TesteMovimentacaoComCategoriaFuncoesExtra.java
@@ -0,0 +1,62 @@
+package br.alura.com.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.alura.com.financas.Util.JPAUtil;
+import br.alura.com.financas.modelo.Categoria;
+import br.alura.com.financas.modelo.Conta;
+import br.alura.com.financas.modelo.Movimentacao;
+import br.alura.com.financas.modelo.TipoMovimentacao;
+
+public class TesteMovimentacaoComCategoriaFuncoesExtra {
+
+	public static void main(String[] args) {
+		Categoria categoria1 = new Categoria("Categoria1");
+		Categoria categoria2 = new Categoria("Categoria2");
+		
+		Conta conta = new Conta();
+		conta.setId(1);
+		
+		
+		Movimentacao movimentacao1 = new Movimentacao();
+		movimentacao1.setData(Calendar.getInstance());
+		movimentacao1.setDescricao("Viagem a JP");
+		movimentacao1.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao1.setValor(new BigDecimal("300.00"));
+		movimentacao1.setCategoria(Arrays.asList(categoria1, categoria2));
+		movimentacao1.setConta(conta);
+
+		
+		Movimentacao movimentacao2 = new Movimentacao();
+		
+		Calendar amanha = Calendar.getInstance();
+		amanha.add(Calendar.DAY_OF_MONTH, 1);
+
+		movimentacao2.setData(amanha);
+		movimentacao2.setDescricao("Viagem a RE");
+		movimentacao2.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao2.setValor(new BigDecimal("450.00"));
+		movimentacao2.setCategoria(Arrays.asList(categoria1, categoria2));
+		movimentacao2.setConta(conta);
+		
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+				
+		em.getTransaction().begin();
+		em.persist(categoria1);
+		em.persist(categoria2);
+		
+		em.persist(movimentacao1);
+		em.persist(movimentacao2);
+//		Movimentacao movimentacao = em.find(Movimentacao.class, 5);
+//		movimentacao.setDescricao("Viagem a PE");
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
