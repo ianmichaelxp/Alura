0a312a8a9e4c853939533d99354600b3677dcbbf
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-10-10 11:12:38 -0300

    Exibir todas as movimentaçoes LAZY

---
+++ ALURA - JPA2 & HIBERNATE/src/br/alura/com/financas/teste/TesteTodasMovimentacoes.java
@@ -0,0 +1,36 @@
+package br.alura.com.financas.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.alura.com.financas.Util.JPAUtil;
+import br.alura.com.financas.modelo.Conta;
+
+public class TesteTodasMovimentacoes {
+
+	public static void main(String[] args) {
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		String jpql = "select c from Conta c";
+		Query query = em.createQuery(jpql);
+		
+		List<Conta> tdsContas = query.getResultList();
+		for (Conta conta : tdsContas) {
+			System.out.println("Titular: " + conta.getTitular());
+			System.out.println("Movimentações: ");
+			System.out.println(conta.getMovimentacoes());
+		}
+		
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	
+	
+	
+
+}
